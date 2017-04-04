a5a689cac62670920fcf501df10aa4ad36b89aaa
js296 <js296@njit.edu>
2017-04-04 15:54:08 -0400

    Adding PDO db connection

---
+++ week11a.php
@@ -0,0 +1,18 @@
+<?php
+
+	include_once 'week11-pdoConnect.php';
+	
+	$result = prepareAndExecute('SELECT * FROM accounts WHERE email = ?', array("js296@njit.edu")); 
+
+	if(!$result) { //if account does not exist
+	    header('HTTP/1.1 400 Bad Request');
+	    exit("ERROR: Email doesn't exist.");
+	} else if ($result['activated'] != 1) {
+		header('HTTP/1.1 400 Bad Request');
+		exit("ERROR: Account has not been activated yet.");
+	} else {
+		echo $result['message'];
+	}
+
+
+?>
